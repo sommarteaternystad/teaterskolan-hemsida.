de919a68a762bfcbc5c59b0923f5af29c979d757
sommarteaternystad <sommarteatern@sommarteaternystad.com>
2026-03-18 00:15:33 +0100

    Create site.js

---
+++ js/site.js
@@ -0,0 +1,8 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const siteNav = document.querySelector('.site-nav');
+
+if (menuToggle && siteNav) {
+  menuToggle.addEventListener('click', () => {
+    siteNav.classList.toggle('open');
+  });
+}
